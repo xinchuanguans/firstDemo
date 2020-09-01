962e1f7b378f35cfba2e9f165ee83cb602bf69b5
xcg <173727578@qq.com>
2020-09-01 20:37:46 +0800

    我的第一个小样

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="nav-footer">
     <div class="service">
-      <div><a>这里</a></div>
-      <div><a>这里</a></div>
+      <div><a>这里可以放版权</a></div>
+      <div><a>这里也可以放外部链接</a></div>
     </div>
   </div>
 </template>
